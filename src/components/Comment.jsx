8097358c75867012e5e0e321ce25218d1d0858bf
Andrea Rodrigues <andrea.melissa.rodrigues@gmail.com>
2022-11-02 13:34:26 +0000

    update comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Comment({ comment }) {
     return (
         <div className="my-2 p-4 border-b-2 border-b-gray-200">
-            <p>{comment.message}</p>
-            <p className="text-sm text-gray-400">Commented By: {comment.creator}</p>
+            <p>{comment.UserResponse}</p>
+            <p className="text-sm text-gray-400">Commented By: {comment.UserName}</p>
         </div>
     )
 }
