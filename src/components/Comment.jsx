8aa6232bd29bb50a69c66f146ab3d6ee87e80a59
Andrea Rodrigues <andrea.melissa.rodrigues@gmail.com>
2022-11-02 13:36:58 +0000

    multi line comment styling

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Comment({ comment }) {
     return (
         <div className="my-2 p-4 border-b-2 border-b-gray-200">
-            <p>{comment.UserResponse}</p>
+            <p className="whitespace-pre">{comment.UserResponse}</p>
             <p className="text-sm text-gray-400">Commented By: {comment.UserName}</p>
         </div>
     )
